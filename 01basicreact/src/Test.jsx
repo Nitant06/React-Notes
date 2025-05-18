054b7e1abbb55afb12df5a4b2f5d70ba5df4b338
Nitant <nitantsharma292@gmail.com>
2025-05-19 00:11:44 +0530

    initial commit

---
+++ 01basicreact/src/Test.jsx
@@ -0,0 +1,7 @@
+function Test() {
+    return (
+        <h3>Hello from Test Component of basic react</h3>
+    )
+}
+
+export default Test;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Test from "./Test"
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World from Vite</h1>
+      <Test />
+    </>
+  )
+}
+
+export default App
