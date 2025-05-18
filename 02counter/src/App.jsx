024941e73d744989bbbc4cb3d1c409bdbd2a2034
Nitant <nitantsharma292@gmail.com>
2025-05-19 01:27:58 +0530

    counter code

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(15);
+
+  //let counter = 15;
+
+  const addValue = () => {
+    counter = counter + 1;
+    setCounter(counter);
+  }
+
+  const removeValue = () => {
+    counter = counter - 1;
+    setCounter(counter);
+  }
+
+  return (
+    <>
+      <h1>Hooks in React</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add Value</button>
+
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
